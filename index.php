348f6a91b1cec4ba8a157e20c2b784e1eb9cc99c
marethyun <bacci.ange@gmail.com>
2019-11-02 13:31:31 +0100

    Drafted something

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ ini_set("xdebug.var_display_max_depth", -1);
 spl_autoload_register(function ($className) {
     // The classname contains the class' namespace name, so they're imported successfully as they're contained in directories named after theirs namespaces
     // (what a long sentence, eh ?)
-    require __DIR__ . '/' . $className . '.php';
+    require str_replace('\\', '/', __DIR__ . '/' . $className . '.php');
 
 });
 
