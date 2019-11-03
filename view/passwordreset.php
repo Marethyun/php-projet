1092a4f583a6ad3afebbe97d92381fe83c71d003
marethyun <bacci.ange@gmail.com>
2019-11-03 17:06:21 +0100

    Worked on password reset view

--- view/passwordreset.php
+++ view/passwordreset.php
@@ -1,23 +1,33 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
     <head>
-        <title>Reset password</title>
-        <meta charset="UTF-8">
+        <title>FreeNote - Réinitialiser un mot de passe</title>
+        <?php include_once 'head.php' ?>
     </head>
+
     <body>
-    <?php if (isset($GLOBALS[DATASET_ENTRY]['error'])) { ?>
-        <p style="color: red"><?= $GLOBALS[DATASET_ENTRY]['error'] ?></p>
-    <?php } ?>
-        <form action="<?= \core\Router::getInstance()->routeUri(ROUTE_RESET, array('token' => $GLOBALS[DATASET_ENTRY]['reset_token'])) ?>" method="post">
-            <label>
-                Password
-                <input type="password" name="password">
-            </label>
-            <label>
-                Repeat password
-                <input type="password" name="password_repeat">
-            </label>
-            <input type="hidden" name="reset_token" value="<?= $GLOBALS[DATASET_ENTRY]['reset_token'] ?>">
-            <input type="submit">
-        </form>
+
+    <main>
+        <div class="boxreini">
+            <h1 class="hform">Réinitialiser votre Mot de Passe</h1>
+            <form class="modifmdpform" action="<?= \core\Router::getInstance()->routeUri(ROUTE_RESET, array('token' => $GLOBALS[DATASET_ENTRY]['reset_token'])) ?>" method="POST">
+                <label for="password">Nouveau Mot de Passe</label>
+                <input type="password" name="password" id="password"/>
+
+                <label for="password">Verifier votre Nouveau Mot de Passe</label>
+                <input type="password" name="password_repeat" id="password"/>
+
+                <input type="hidden" name="reset_token" value="<?= $GLOBALS[DATASET_ENTRY]['reset_token'] ?>">
+
+                <?php if (isset($GLOBALS[DATASET_ENTRY]['error'])) { ?>
+                    <p style="color: red"><?= $GLOBALS[DATASET_ENTRY]['error'] ?></p>
+                <?php } ?>
+
+                <input type="submit" name="action" value="Enregistrer les modifications" id="mailer">
+            </form>
+        </div>
+    </main>
+    <?php include_once 'footer.php' ?>
     </body>
+
 </html>
