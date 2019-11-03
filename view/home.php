adc9666d8f55455129955901d8ac53393796b8d3
marethyun <bacci.ange@gmail.com>
2019-11-03 15:42:50 +0100

    Worked on homepage

--- view/home.php
+++ view/home.php
@@ -1,63 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <meta charset="UTF-8">
-        <title>Accueil</title>
-    </head>
-    <body>
-    <?php if (isset($GLOBALS[DATASET_ENTRY]['error'])) { ?>
-        <p style="color: red"><?= $GLOBALS[DATASET_ENTRY]['error'] ?></p>
-    <?php } ?>
-        <h1>Bienvenue !</h1>
-
-    <?php if (\core\Session::isLogged()) { ?>
-        <form action="<?= \core\Router::getInstance()->routeUri(ROUTE_HOME) ?>" method="post">
-            <input type="hidden" name="action" value="new_thread">
-            <input type="submit" value="Nouvelle discussion">
-        </form>
-    <?php } ?>
-        <h2>Discussions</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Identifiant</th>
-                    <th>Dernier message</th>
-                    <th>État</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($GLOBALS[DATASET_ENTRY]['threads'] as $thread) { ?>
-                <tr>
-                    <td style="font-family: monospace"><a href="<?= \core\Router::getInstance()->routeUri(ROUTE_THREAD, array('thread' => \model\wrappers\Ids::toHex($thread->id))) ?>">#<?= \model\wrappers\Ids::toHex($thread->id) ?></a></td>
-                    <td>
-                        <?php if(empty($thread->messages)) { ?>(Discussion vide)<?php }?>
-                        <?php if(empty(end($thread->messages)->fragments)) {?>
-                            <?php if (!$thread->opened and count($thread->messages) >= 2) {?>
-                                <?php foreach ($thread->messages[count($thread->messages) - 2]->fragments as $fragment) { ?>
-                                    <?= $fragment->content ?>
-                                <?php } ?>
-                            <?php } else { ?>
-                                (Discussion à compléter)
-                            <?php } ?>
-                        <?php } else { ?>
-                            <?php foreach (end($thread->messages)->fragments as $fragment) { ?>
-                                <?= $fragment->content ?>
-                            <?php } ?>
-                        <?php } ?>
-                    </td>
-                    <td><?= $thread->opened ? 'Ouverte' : 'Fermée' ?></td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
-    </body>
-</html>
-
-<!-- TODO -->
-
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
         <title>FreeNote - Accueil</title>
         <?php include_once 'head.php' ?>
     </head>
