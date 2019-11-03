11e29ca0c6478e3849d06978e2af5933208b8a0a
marethyun <bacci.ange@gmail.com>
2019-11-03 17:26:56 +0100

    Worked on errors

--- view/400.php
+++ view/400.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>400</title>
-    </head>
-    <body>
-        <h1>400 Bad request</h1>
-    </body>
+<html lang="fr">
+<head>
+    <title>FreeNote - 400</title>
+    <?php include_once 'head.php' ?>
+</head>
+
+<body>
+<div id="erreur">
+    <h1 class="hform">400 - Bad request</h1>
+
+    <p>Votre requête semble présenter des problèmes.</p>
+</div>
+</body>
+
 </html>
